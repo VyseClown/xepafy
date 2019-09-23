ffe258eeca93c9c97a045a89ac58153aba0f9068
Alessandro <vyseclown@gmail.com>
2019-09-22 22:03:10 -0300

    Quase metade feita!

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+export default function Card({ data }) {
+  return (
+    <Container>
+      <header>
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
+      </header>
+      <p>{data.content}</p>
+      <img src={data.user} alt="" />
+    </Container>
+  );
+}
