2f87dabfffc27b9a7213059b8c55a26cdb36c2ef
Alessandro <vyseclown@gmail.com>
2019-09-24 21:37:23 -0300

    Antes de fazer a mudança usando useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
@@ -6,12 +8,12 @@ import Board from "./components/Board";
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
 
       <GlobalStyle />
-    </>
+    </DndProvider>
   );
 }
 
